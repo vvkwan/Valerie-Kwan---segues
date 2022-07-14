13bf9618bba8194575f1c42a6ae1051f0f60ca14
Valerie Kwan <valvalkwan@gmail.com>
2022-07-14 10:08:31 -0700

    Initial Commit

---
+++ Valerie Kwan - segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Valerie Kwan - segues
+//
+//  Created by Scholar on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
